6b8c597264c648de018c80c4898d074317c21785
M KAYANI <mmkayani@hotmail.co.uk>
2023-10-02 16:26:06 +0100

    Updated App.js Updated ExpenseDate.js Updated ExpenseItem.js

--- frontend/src/components/ExpenseItem.js
+++ frontend/src/components/ExpenseItem.js
@@ -1,25 +1,17 @@
+import ExpenseDate from './ExpenseDate';
 import './ExpenseItem.css';
 
 function ExpenseItem(props) {
-  const month = props.date.toLocaleString('en-US', {month: 'long'});
-  const day = props.date.toLocaleString('en-US', {day: '2-digit'});
-  const year = props.date.getFullYear();
-  
-  
   return (
         <div classname='expense-item' >
-            <div>
-              <div>{month}</div>
-             <div>{Year}</div>
-             <div>{Day}</div>
-            </div>
-            <div classname='expence-item__description'>
+          <ExpenseDate date={props.date} />
+            <div classname='expence-item__description' >
               <h2>{props.title}</h2>
               <div classname='expense-item_price'>${props.amount}</div>
           </div>
         </div>
-  );  
-}
+  ) 
+};
 
 export default ExpenseItem;
 
