bbe80ef51962c03a2ed76ec658816f00b3c1a305
M KAYANI <mmkayani@hotmail.co.uk>
2023-10-03 09:31:36 +0100

    frontend/src/components/ExpenseItem.js

--- frontend/src/components/ExpenseItem.js
+++ frontend/src/components/ExpenseItem.js
@@ -3,9 +3,9 @@ import './ExpenseItem.css';
 
 function ExpenseItem(props) {
   return (
-        <div className="expense-item">
+        <div className='expense-item'>
           <ExpenseDate date={props.date}/>
-            <div className="expense-item__description">
+            <div className='expense-item__description'>
               <h2>{props.title}</h2>
               <div className='expense-item__price'>${props.amount}</div>
           </div>
