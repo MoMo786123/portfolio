2cfcabdfef33714060aa5b2a4210fc44ee9a16ca
M KAYANI <mmkayani@hotmail.co.uk>
2023-10-04 16:20:16 +0100

    modified:   Menu/menu/src/components/concept.js

--- Menu/menu/src/components/concept.js
+++ Menu/menu/src/components/concept.js
@@ -1,6 +1,10 @@
 function Concepts() {
     return (
-
+        <li className="concept">
+          <img src={concepts[0].image} alt={concepts[0].title} />
+          <h2>{concepts[0].title}</h2>
+          <p>{concepts[0].description}</p>
+        </li>
     )
 }
 
